10ac52d33a082e9fb10234e2871bdc4364e99d16
Fellipe Lima <140414505+francafellipe@users.noreply.github.com>
2023-09-23 18:59:47 -0300

    Add files via upload

---
+++ ProvaAlgoritmoseVariaveis.py
@@ -0,0 +1,13 @@
+#var
+#exemplos de principais tipagens de variaveis 
+idade = 27
+nome = 'Fellipe'
+altura = 1.83
+verdadeiro = True
+falso = False
+
+solicitacao = input('Qual o seu nome  :')
+#inicio
+print(f'Seja bem vindo  {solicitacao}')
+
+#fimalgoritmo
